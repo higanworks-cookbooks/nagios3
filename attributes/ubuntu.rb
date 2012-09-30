96876609098f61286ef748ec13066e9ba3699c25
sawanoboly <sawanoboriyu@higanworks.com>
2012-09-30 16:18:21 +0900

    test: attribute for ubuntu

--- attributes/ubuntu.rb
+++ attributes/ubuntu.rb
@@ -1,2 +1,2 @@
-default["nagios_command_path"] = "/usr/sbin/nagios3"
-default["nagios_plugins_dir"] =  "/usr/lib/nagios/plugins"
+node.default["nagios3"]["nagios_command_path"] = "/usr/sbin/nagios3"
+node.default["nagios3"]["nagios_plugins_dir"] =  "/usr/lib/nagios/plugins"
